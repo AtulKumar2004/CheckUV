8a5f6037b0ad3e0437e7d0becdf0187d4ecd5e1e
AtulKumar2004 <akdeoghar16@gmail.com>
2024-10-01 05:56:47 +0530

    Change some parts

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import fetch from "node-fetch";
 import axios from "axios";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
